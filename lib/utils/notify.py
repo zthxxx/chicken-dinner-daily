e8d09a9b41c91c9d943392c9f6a363e28fd96d39
zthxxx <zthxxx.me@gmail.com>
2018-10-24 20:11:07 +0800

    refactor(notify): alter notify function to wait background process finish

--- lib/utils/notify.py
+++ lib/utils/notify.py
@@ -1,9 +1,7 @@
-import time
 import pync
 
 
 def notify(title, message):
-    pync.notify(message, title=title, open='https://google.com/', sender='com.apple.launchpad.launcher')
-    # sleep for call terminal-notifier
-    # https://stackoverflow.com/questions/37010132/launchd-python-notifier-notify-not-producing-expected-output
-    time.sleep(0.1)
+    # wait for call terminal-notifier
+    # https://stackoverflow.com/questions/37010132/launchd-python-notifier-notify-not-producing-expected-output/52968611#52968611
+    pync.notify(message, title=title, open='https://google.com/', sender='com.apple.launchpad.launcher', wait=True)
