f3ab97b4045db5a55370a32c324f4b8685d2d5ae
zthxxx <zthxxx.me@gmail.com>
2018-10-24 19:00:31 +0800

    fix(notify): fix run termain-notifier by launchd
    ref: https://stackoverflow.com/questions/37010132/launchd-python-notifier-notify-not-producing-expected-output

--- lib/utils/notify.py
+++ lib/utils/notify.py
@@ -1,5 +1,9 @@
+import time
 import pync
 
 
 def notify(title, message):
     pync.notify(message, title=title, open='https://google.com/', sender='com.apple.launchpad.launcher')
+    # sleep for call terminal-notifier
+    # https://stackoverflow.com/questions/37010132/launchd-python-notifier-notify-not-producing-expected-output
+    time.sleep(0.1)
